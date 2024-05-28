ba3b98ff5346f2b7d6846973acca531742fe2495
Alexander <alex@alexvoiceover.com>
2024-05-28 20:32:02 +0100

    Edited info

--- scripts.js
+++ scripts.js
@@ -1,40 +1,76 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     var emailContainer = document.getElementById('email-container');
-//     if (emailContainer) {
-//         // Encoded email address
-//         var encodedEmail = "aGFqaXk1ODgwOEBmaW5jYWluYy5jb20=";
-//         // Decode the email address
-//         var decodedEmail = atob(encodedEmail);
-//         var emailLink = document.createElement('a');
-//         emailLink.href = 'mailto:' + decodedEmail;
-//         emailLink.textContent = 'Email me';
-        
-//         emailLink.classList.add('email-button'); // Apply the CSS class for styling
-        
-//         emailLink.addEventListener('mouseover', function() {
-//             emailLink.style.backgroundColor = '#a51f62';
-//         });
+document.addEventListener('DOMContentLoaded', function () {
+	var emailContainer = document.getElementById('email-container');
+	if (emailContainer) {
+		// Encoded email parts in reverse order
+		var encodedEmail = 'moc.cnicnif@80885yijah'.split('').reverse().join('');
 
-//         emailLink.addEventListener('mouseout', function() {
-//             emailLink.style.backgroundColor = 'var(--text-color)';
-//         });
+		var emailSpan = document.createElement('span');
+		emailSpan.style.unicodeBidi = 'bidi-override';
+		emailSpan.style.direction = 'rtl';
+		emailSpan.innerHTML = encodedEmail
+			.split('')
+			.map(function (char, index) {
+				return index === 11 ? '<span style="display:none">foo</span>' + char : char;
+			})
+			.join('');
 
-//         emailContainer.appendChild(emailLink);
-//     }
-// });
-document.addEventListener("DOMContentLoaded", function() {
-    var emailContainer = document.getElementById('email-container');
-    if (emailContainer) {
-        // Encoded email parts in reverse order
-        var encodedEmail = "moc.cnicnif@80885yijah".split("").reverse().join("");
-        
-        var emailSpan = document.createElement('span');
-        emailSpan.style.unicodeBidi = 'bidi-override';
-        emailSpan.style.direction = 'rtl';
-        emailSpan.innerHTML = encodedEmail.split('').map(function(char, index) {
-            return index === 11 ? '<span style="display:none">foo</span>' + char : char;
-        }).join('');
-        
-        emailContainer.appendChild(emailSpan);
-    }
+		emailContainer.appendChild(emailSpan);
+	}
 });
+
+(function () {
+	var a, b, c, e, i, f, p, w, g, z, s, r, l;
+	z = document;
+	s = z.querySelector.bind(z);
+	r = Math.random().toString().substring(2);
+	l = s('.email-loader-container');
+	l.className = '';
+	l.id = 'e' + r;
+	z = '#e' + r;
+	window.addEventListener('load', function () {
+		g = s(z + '>canvas');
+		w = s(z + '>img');
+		if (w.complete && w.naturalHeight !== 0) {
+			q();
+		} else {
+			w.onload = q;
+		}
+		function q() {
+			c = g.width = g.height = w.width;
+			f = g.getContext('2d', { willReadFrequently: true });
+			f.drawImage(w, 0, 0);
+			b = '';
+			p = String.fromCharCode;
+			w = parseInt;
+			e = f.getImageData(0, 0, c, c).data;
+			r = [];
+			c = 0;
+			for (i = 0; i < e.length; i++) {
+				c++;
+				if (c == 4) {
+					c = 0;
+					if (e[i] == 0) {
+						break;
+					}
+					continue;
+				}
+				r.push(e[i] > 127 ? 1 : 0);
+				if (r.length >= 7) {
+					b += p(w(r.splice(0, 7).join(''), 2));
+				}
+			}
+			try {
+				for (i = 0; i < 100; i++) {
+					b = atob(b);
+				}
+			} catch (g) {
+				a = '';
+				for (i = 0; i < b.length; i += 3) {
+					c = p(w(b.substr(i, 3).replace(/\./, ''), 10));
+					a += c;
+				}
+			}
+			l.outerHTML = a;
+		}
+	});
+})();
